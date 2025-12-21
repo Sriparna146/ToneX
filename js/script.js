ed601f306ef91920c88c7d3f01f5e7d14aaebd46
Sriparna146 <haldersriparno6@gmail.com>
2025-12-21 12:40:51 +0530

    Building Playlist

---
+++ js/script.js
@@ -0,0 +1,45 @@
+console.log("Script loaded");
+
+async function getsongs() {
+  let a = await fetch("http://127.0.0.1:3000/songs/")
+  let response = await a.text();
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("a");
+  
+  let songs = [];
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href.split("/%5Csongs%5C")[1]);
+    }
+  }
+  return songs;
+}
+
+async function main() {
+
+  // Get the list of songs
+  let songs = await getsongs();
+  console.log(songs);
+
+  // Show all the songs into playlist
+  let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
+  for (const song of songs) {
+    songUL.innerHTML = songUL.innerHTML + `<li><img class="invert" src="music.svg" alt="">
+                                <div class="info">
+                                    <div> ${song.replaceAll("%20", " ")}</div>
+                                    <div>Song Rony</div>
+                                </div>
+                                <div class="playnow">
+                                    <span>Play Now</span>
+                                    <img class="invert" src="play.svg" alt="">
+                                </div>
+                            </li>`;
+  }
+  //play the first song
+  var audio = new Audio(songs[0]);
+  audio.play();  
+}
+
+main();
